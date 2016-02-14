3ba1cd98a0021bdb77dbb0f0bf18468a3a5e297d
Charles Yeh <charlesyeh@dropbox.com>
2016-02-13 17:09:10 -0800

    show categories, and filter headers

--- Yelp/FilterTableViewCell.swift
+++ Yelp/FilterTableViewCell.swift
@@ -8,7 +8,16 @@
 
 import UIKit
 
+@objc protocol SwitchCellDelegate {
+    optional func switchCell(switchCell: FilterTableViewCell, didChangeValue value: Bool)
+}
 class FilterTableViewCell: UITableViewCell {
     @IBOutlet weak var cellLabel: UILabel!
     @IBOutlet weak var cellSwitch: UISwitch!
+    
+    var delegate: SwitchCellDelegate?
+    
+    @IBAction func switchValueChanged(sender: AnyObject) {
+        delegate?.switchCell?(self, didChangeValue: cellSwitch.on)
+    }
 }
